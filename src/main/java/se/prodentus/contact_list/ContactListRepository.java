af5ca00e416c01a8d7a20c107884ec6b6414370b
Christian Löwendahl <x@x.se>
2017-05-24 14:40:26 +0200

    added repo

---
+++ src/main/java/se/prodentus/contact_list/ContactListRepository.java
@@ -0,0 +1,12 @@
+package se.prodentus.contact_list;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactListRepository extends JpaRepository<Contact, Long>{
+	List<Contact> findByFirstNameIgnoreCase(String firstName);
+	List<Contact> findByLastNameIgnoreCase(String lastName);
+	List<Contact> findByFirstNameContainingIgnoreCase(String firstNameSearchWord);
+	List<Contact> findByLastNameContainingIgnoreCase(String lastNameSearchWord);
+}
