79d5ad2d5d6e4c318fa46b8196b5b7496163fdcf
Christian Löwendahl <x@x.se>
2017-05-31 15:19:11 +0200

    Minor adjustments.

--- src/main/java/se/prodentus/contact_list/ContactListApplication.java
+++ src/main/java/se/prodentus/contact_list/ContactListApplication.java
@@ -1,5 +1,6 @@
 package se.prodentus.contact_list;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +8,24 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ContactListApplication {
+public class ContactListApplication implements CommandLineRunner {
+	
+	@Autowired
+	private ContactListRepository contactListRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactListApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		contactListRepository.save(new Contact("Anders", "Andersson", "010-111 11 11", "anders@prodentus.se"));
+		contactListRepository.save(new Contact("Berit", "Bengtsson", "020-222 22 22", "berit@prodentus.se"));
+		contactListRepository.save(new Contact("Carl", "Carlsson", "030-333 33 33", "carl@prodentus.se"));
+		contactListRepository.save(new Contact("Diana", "Davidsson", "040-444 44 44", "diana@prodentus.se"));
+	}
 	
+	/**
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext context, ContactListRepository contactListRepository) {
 		return args -> {
@@ -22,5 +35,7 @@ public class ContactListApplication {
 			contactListRepository.save(new Contact("Diana", "Davidsson", "040-444 44 44", "diana@prodentus.se"));
 		};
 	}
+	
+	*/
 	
 }
